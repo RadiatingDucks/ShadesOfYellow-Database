f037e1580911a962b10d9fd6e499e671b597753f
Sihouette-Of-Past <23751@burnside.school.nz>
2025-05-02 22:50:00 +1200

    Tested database with basic code

--- shadesOfYellowdb.py
+++ shadesOfYellowdb.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+import sqlite3
+
+db = sqlite3.connect("shadesOfYellow.db")
+
+cursor = db.cursor()
+
+#SQL code
+sql = "SELECT * FROM shadesOfYellow"
+
+cursor.execute(sql)
+
+results = cursor.fetchall()
+
+print(results)
+
+db.close
