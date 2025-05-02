2e5a15c9364e6679fe43db39f7259cbc7bb0495e
Sihouette-Of-Past <23751@burnside.school.nz>
2025-05-02 22:57:22 +1200

    Added docstring and added comments for sections

--- shadesOfYellowdb.py
+++ shadesOfYellowdb.py
@@ -1,6 +1,18 @@
+"""Code with functions for database of shades of yellow by Amy Lian 5/2/2025"""
+
+#imports
+
 import sqlite3
 
-db = sqlite3.connect("shadesOfYellow.db")
+#constants and variables
+
+DATABASE = "shadesOfYellow.db"
+
+#functions
+
+#main
+
+db = sqlite3.connect(DATABASE)
 
 cursor = db.cursor()
 
